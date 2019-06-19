792838751d526062a90e3c81b339e5b4e615838d
Matus <matus.ozaniak@profiq.com>
2019-06-19 16:20:55 +0200

    firefox

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 jv=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
-ch=$(chromium-browser --version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1 | sed '/Chromium /s///')
-if [ $jv -ge 8 ] && [ $ch -ge 73 ]; then
+ff=$(firefox -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1 | sed '/Mozilla Firefox /s///')
+
+if [ $jv -ge 8 ] && [ $ff -ge 66 ]; then
     echo ok
     exit 0
 else
